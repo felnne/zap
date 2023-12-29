ec9f51f76e191515e94135994e21566c72d7d325
Felix Fennell <felnne@bas.ac.uk>
2023-12-29 15:29:52 +0000

    Fixing model binding for Textarea base component

--- src/components/FormTextarea.vue
+++ src/components/FormTextarea.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const model = defineModel() as unknown as string | number | undefined
+</script>
+
 <template>
   <textarea
     class="bg-white dark:bg-gray-800 border border-black dark:border-white text-black dark:text-white"
+    v-model="model"
   ></textarea>
 </template>
