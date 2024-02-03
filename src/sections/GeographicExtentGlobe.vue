414d7ab42800bab348c3f34eeb60995473829b51
Felix Fennell <felnne@bas.ac.uk>
2024-02-03 14:55:56 +0000

    Adding logic to hide extent globe without token

--- src/sections/GeographicExtentGlobe.vue
+++ src/sections/GeographicExtentGlobe.vue
@@ -4,6 +4,8 @@ import { nextTick, onMounted, type PropType, watch } from 'vue'
 import type { EsriToken, WellKnownExtent } from '@/types/app'
 import { initExtentGlobe, loadCssTheme } from '@/utils/esriNoTest'
 
+import Link from '@/components/Link.vue'
+
 const props = defineProps({
   wke: {
     type: Object as PropType<WellKnownExtent>,
@@ -17,6 +19,7 @@ const props = defineProps({
 
 const container = 'geographic-extent-globe'
 
+const classes = 'h-96 w-full border border-sky-500'
 
 watch(
   () => props.esriToken,
@@ -34,5 +37,16 @@ onMounted(() => {
 </script>
 
 <template>
+  <div
+    v-if="!esriToken"
+    class="flex items-center justify-center text-lg"
+    :class="classes"
+    :id="container"
+  >
+    <p>
+      Sign in to ArcGIS Online in <Link href="#external-services">External Services</Link> to show
+      3D preview.
+    </p>
   </div>
+  <div v-else :class="classes" :id="container"></div>
 </template>
