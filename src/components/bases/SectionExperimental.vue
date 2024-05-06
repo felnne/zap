8d66714d40b23b9df29c019ed1b11c193f75c78e
Felix Fennell <felnne@bas.ac.uk>
2024-05-06 12:00:04 +0100

    Adding experimental section component
    For future use

---
+++ src/components/bases/SectionExperimental.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const show = ref(false)
+</script>
+
+<template>
+  <template v-if="show">
+    <slot />
+  </template>
+  <template v-else="!show">
+    <div
+      @click="show = true"
+      class="app-diagonalstripes-orange-100 flex cursor-pointer items-center justify-center bg-orange-300 p-20"
+    >
+      <div class="inline-block space-y-4 bg-white bg-opacity-70 p-4 text-center dark:bg-black">
+        <p class="text-3xl">Here be dragons 🏴󠁧󠁢󠁷󠁬󠁳󠁿</p>
+        <p>This section is super experimental so may not work, at all.</p>
+        <p>To probably be disappointed, click anywhere.</p>
+      </div>
+    </div>
+  </template>
+</template>
