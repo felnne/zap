7f06343c4a464fac4342d1bafb9d98029a1b4a19
Felix Fennell <felnne@bas.ac.uk>
2024-05-06 23:29:33 +0100

    Adding initial OpenAI method
    This is not a safe way of using the SDK since it’s accessible client side, however we control (broadly) who can access the content so is ok enough for a review app.
    
    Will need to move the actual client calls into a proxy app somewhere prior to merging.

--- src/components/sections/elements/Summary.vue
+++ src/components/sections/elements/Summary.vue
@@ -3,13 +3,15 @@ import { ref, watch } from 'vue'
 
 import { Stability } from '@/types/enum'
 import type { DropdownItem } from '@/types/app'
+import { summariseAbstract } from '@/lib/ai'
 
 import SectionBorder from '@/components/bases/SectionBorder.vue'
 import SectionTitle from '@/components/bases/SectionTitle.vue'
-import SectionExperimental from '@/components/bases/SectionExperimental.vue'
+import GuidanceText from '@/components/bases/GuidanceText.vue'
 import Markdown from '@/components/bases/Markdown.vue'
+import Button from '@/components/bases/Button.vue'
 
-defineProps({
+const props = defineProps({
   abstract: {
     type: String,
     required: true,
@@ -21,9 +23,19 @@ const emit = defineEmits<{
   'update:isoPurpose': [id: string]
 }>()
 
+const copyFromAbstract = () => {
+  markdownInput.value = props.abstract
+}
+
+const summariseFromAbstract = async () => {
+  markdownInput.value = '[Generating summary...]'
+  markdownInput.value = await summariseAbstract(props.abstract)
+}
+
 const dependantSections: DropdownItem[] = [{ href: '#access', title: 'Access Restrictions' }]
 
-const summary = ref<string>('')
+let summary = ref<string>('')
+let markdownInput = ref<string>('')
 
 watch(
   () => summary.value,
@@ -45,8 +57,33 @@ watch(
       :depends-on="dependantSections"
     />
     <div class="space-y-4">
-      <Markdown input-id="summary-input" @update:input="(event: string) => (summary = event)" />
-      <SectionExperimental><p>Coming Soon!</p></SectionExperimental>
+      <div class="flex items-center space-x-2">
+        <Button id="summary-use-abstract" @click="copyFromAbstract" :disabled="props.abstract == ''"
+          >Copy From Abstract</Button
+        >
+        <GuidanceText
+          >Click to copy the abstract (if set) into the input below (replacing any existing
+          value).</GuidanceText
+        >
+      </div>
+      <div class="flex items-center space-x-2">
+        <Button
+          id="summary-use-abstract-ai"
+          @click="summariseFromAbstract"
+          :disabled="props.abstract == ''"
+        >
+          Summarise From Abstract
+        </Button>
+        <GuidanceText
+          >Click to summary the abstract (if set) <strong>✨ using the power of AI ✨</strong> into
+          the input below (replacing any existing value).</GuidanceText
+        >
+      </div>
+      <Markdown
+        input-id="summary-input"
+        @update:input="(event: string) => (summary = event)"
+        :input="markdownInput"
+      />
     </div>
   </SectionBorder>
 </template>
