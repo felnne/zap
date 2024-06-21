6eadb7f5880018d23a681c7254cde6bc81464905
Felix Fennell <felnne@bas.ac.uk>
2024-06-21 11:33:57 +0100

    Fixing format linting

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -10,24 +10,27 @@ export async function summariseAbstract(abstract: string): Promise<string> {
    */
   const url = getSetting('app_open_ai_proxy_endpoint')
   const model = getSetting('app_open_ai_model')
-  const prompt = "You help create discovery metadata, that is information that allows prospective users of data products to evaluate whether they’ll meet their needs (what it’s about, recent enough, in the right area, etc.). Specifically you will be provided with abstracts for products and you will return (with no other information) a summary version that is 2 (3 at most) short sentences for use in search results. NEVER say who manages/sponsors a product or start with variations of 'This dataset provides'."
+  const prompt =
+    "You help create discovery metadata, that is information that allows prospective users of data products to evaluate whether they’ll meet their needs (what it’s about, recent enough, in the right area, etc.). Specifically you will be provided with abstracts for products and you will return (with no other information) a summary version that is 2 (3 at most) short sentences for use in search results. NEVER say who manages/sponsors a product or start with variations of 'This dataset provides'."
 
   const data = {
-    "model": model,
-    "messages": [
+    model: model,
+    messages: [
       {
-        "role": "system",
-        "content": prompt
+        role: 'system',
+        content: prompt,
       },
       {
-        "role": "user",
-        "content": abstract
-      }
-    ]
+        role: 'user',
+        content: abstract,
+      },
+    ],
   }
 
   try {
-    const response = await axios.post(url, data, {headers: {'Content-Type': 'application/json',}})
+    const response = await axios.post(url, data, {
+      headers: { 'Content-Type': 'application/json' },
+    })
     return response.data.choices[0].message.content
   } catch (error: any) {
     throw new Error('OpenAI Proxy error')
