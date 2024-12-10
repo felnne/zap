5281db6167adf97774124054b88dcc7f5afa1fac
Felix Fennell <felnne@bas.ac.uk>
2024-12-10 13:21:09 +0000

    Fixing SAN selection

--- src/components/bases/Upload.vue
+++ src/components/bases/Upload.vue
@@ -33,19 +33,20 @@ let sizeBytes = ref<number | undefined>(undefined)
 let url = ref<string | undefined>(undefined)
 
 let upload: ComputedRef<Upload | undefined> = computed(() => {
-  if (!source.value || !format.value || !sizeBytes.value) return undefined
+  if (!source.value || !format.value) return undefined
   const url_ = url.value ? url.value : ''
+  const size_ = sizeBytes.value ? sizeBytes.value : 0
 
   return {
     source: source.value,
     format: format.value,
-    sizeBytes: sizeBytes.value,
+    sizeBytes: size_,
     url: url_,
   }
 })
 
 watch(
-  () => format.value,
+  () => [format.value, url.value],
   () => {
     if (upload.value) {
       emit('update:upload', upload.value)
