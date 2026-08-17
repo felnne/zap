d6c39bac0ace129053dc0f34d86b8f04f082467c
Felix Fennell <felnne@bas.ac.uk>
2026-08-17 12:08:08 +0100

    Refactoring validation section
    To split validation logic from Streamlit UI.
    
    Applying the same component type isolation to make things easier to test and decouple.
    
    Also, splitting out MAGIC profiles (discovery and administration) as records may comply with one but not the other.

--- src/zap/sections/tool_validate.py
+++ src/zap/sections/tool_validate.py
@@ -1,12 +1,101 @@
+from dataclasses import dataclass
+
 import streamlit as st
+from cattrs import ClassValidationError
 from lantern.lib.metadata_library.models.record.record import Record, RecordInvalidError, RecordSchema
 from lantern.models.record.record import Record as RecordCatalogue
 
 from zap.sections.base import Section
+from zap.utils import flatten_exceptions
+
+
+@dataclass
+class ValidationOutput:
+    """Format record validation check for Streamlit."""
+
+    domain: str
+    status: bool
+    message: str
+    exception: BaseException | None = None
+
+    def render(self) -> None:
+        """Render validation check."""
+        if self.status:
+            st.success(self.message, icon="✅")
+            return
+        st.error(self.message, icon="❌")
+        with st.expander(f"{self.domain} errors") as expander:
+            if isinstance(self.exception, BaseExceptionGroup):
+                for e in flatten_exceptions(self.exception):
+                    expander.exception(e)
+                return
+            expander.exception(self.exception)
+        return
 
 
 class ToolValidate(Section):
     """Current record validation status."""
+
+    @staticmethod
+    def _validate_base(record: Record) -> ValidationOutput:
+        """Validate against base (lib) record class."""
+        _domain = "Base schema"
+        try:
+            record.validate(use_profiles=False)
+            return ValidationOutput(domain=_domain, status=True, message="Record config is valid 🙂")
+        except RecordInvalidError as e:
+            return ValidationOutput(domain=_domain, status=False, message="Record config is invalid 😭", exception=e)
+
+    @staticmethod
+    def _validate_lantern(record: Record) -> ValidationOutput:
+        """Validate against catalogue record class."""
+        _domain = "Lantern 🏮 catalogue requirements"
+        _link = "[Lantern 🏮 Catalogue Requirements](https://github.com/antarctica/lantern/blob/main/docs/models.md#record-requirements)"
+
+        try:
+            cat_record = RecordCatalogue.loads(record.dumps(strip_admin=False))
+        except ClassValidationError as e_group:
+            return ValidationOutput(
+                domain=_domain,
+                status=False,
+                message="Record cannot be parsed as a catalogue record and cannot be validated further 😵",
+                exception=e_group,
+            )
+        try:
+            cat_record.validate(use_profiles=False)
+            return ValidationOutput(domain=_domain, status=True, message=f"Record config meets {_link} 😀")
+        except RecordInvalidError as e:
+            return ValidationOutput(
+                domain=_domain, status=False, message=f"Record config does not meet {_link} 😰", exception=e
+            )
+
+    @staticmethod
+    def _validate_discovery(record: Record) -> ValidationOutput:
+        """Validate against MAGIC discovery profile."""
+        _domain = "MAGIC Discovery Profile"
+        _link = "[MAGIC Discovery Profile](https://metadata-standards.data.bas.ac.uk/profiles/magic-discovery/v2)"
+
+        try:
+            record.validate(force_schemas=[RecordSchema.MAGIC_DISCO_V2])
+            return ValidationOutput(domain=_domain, status=True, message=f"Record config meets the {_link} 🤩")
+        except RecordInvalidError as e:
+            return ValidationOutput(
+                domain=_domain, status=False, message=f"Record config does not meet the {_link} 😞", exception=e
+            )
+
+    @staticmethod
+    def _validate_admin(record: Record) -> ValidationOutput:
+        """Validate against MAGIC administration profile."""
+        _domain = "MAGIC Administration Profile"
+        _link = "[MAGIC Administration Profile](https://metadata-standards.data.bas.ac.uk/profiles/magic-administration/v1/)"
+
+        try:
+            record.validate(force_schemas=[RecordSchema.MAGIC_ADMIN_V1])
+            return ValidationOutput(domain=_domain, status=True, message=f"Record config meets the {_link} 🥳")
+        except RecordInvalidError as e:
+            return ValidationOutput(
+                domain=_domain, status=False, message=f"Record config does not meet the {_link} 😰", exception=e
+            )
 
     def render(self) -> None:
         """Render."""
@@ -14,28 +103,19 @@ class ToolValidate(Section):
 
         record = st.session_state.get("record", None)
         if not isinstance(record, Record):
-            st.info("Set record config to enable validation.", icon="ℹ️")  # noqa: RUF001
+            st.info("Set record to enable validation.", icon="ℹ️")  # noqa: RUF001
             return
 
-        try:
-            record.validate(use_profiles=False)
-            st.success("Record config is valid 🙂", icon="✅")
-        except RecordInvalidError as e:
-            st.error("Record config is invalid 😞", icon="❌")
-            st.exception(e)
+        base = self._validate_base(record)
+        base.render()
+        if not base.status:
             return
 
-        cat_record = RecordCatalogue.loads(record.dumps(strip_admin=False))
-        try:
-            cat_record.validate(use_profiles=False)
-            st.success("Record config meets catalogue requirements 😀", icon="✅")
-        except RecordInvalidError as e:
-            st.error("Record config does not meet catalogue requirements 😢", icon="❌")
-            st.exception(e)
+        lantern = self._validate_lantern(record)
+        lantern.render()
 
-        try:
-            cat_record.validate(force_schemas=[RecordSchema.MAGIC_DISCO_V2, RecordSchema.MAGIC_ADMIN_V1])
-            st.success("Record config meets MAGIC profile requirements 🥳", icon="✅")
-        except RecordInvalidError as e:
-            st.error("Record config does not meet MAGIC profile requirements 😭", icon="❌")
-            st.exception(e)
+        discovery = self._validate_discovery(record)
+        discovery.render()
+
+        admin = self._validate_admin(record)
+        admin.render()
--- tests/zap_tests/sections/test_validate.py
+++ tests/zap_tests/sections/test_validate.py
@@ -3,9 +3,107 @@ from lantern.lib.metadata_library.models.record.elements.common import Identifie
 from lantern.lib.metadata_library.models.record.record import Record
 from streamlit.testing.v1 import AppTest
 
+from zap.sections.tool_validate import ToolValidate, ValidationOutput
 
-class TestValidateSection:
-    """Test validation tool section."""
+
+class TestValidationOutput:
+    """Test validation check output formatting."""
+
+    @staticmethod
+    def _app_script(output: ValidationOutput) -> None:
+        from zap.sections.tool_validate import ValidationOutput  # noqa: F401
+
+        output.render()
+
+    @pytest.mark.parametrize(
+        "output",
+        [
+            ValidationOutput(domain="x", status=True, message="x"),
+            ValidationOutput(domain="x", status=False, message="x", exception=ValueError("x")),
+            ValidationOutput(
+                domain="x",
+                status=False,
+                message="x",
+                exception=BaseExceptionGroup("x", [ValueError("y"), ValueError("z")]),
+            ),
+        ],
+    )
+    def test_render(self, output: ValidationOutput):
+        """Can render validation output."""
+        at = AppTest.from_function(self._app_script, kwargs={"output": output})
+        at.run()
+
+        if output.status:
+            assert at.success[0].value == output.message
+            assert len(at.error) == 0
+            assert len(at.exception) == 0
+        else:
+            assert at.error[0].value == output.message
+            assert len(at.success) == 0
+            if isinstance(output.exception, BaseExceptionGroup):
+                assert len(at.exception) == len(output.exception.exceptions)
+            else:
+                assert len(at.exception) == 1
+
+
+class TestValidateSectionLogic:
+    """Test validation tool logic."""
+
+    @pytest.mark.parametrize(("state", "status"), [("valid", True), ("invalid", False)])
+    def test_validate_base(self, fx_record_config_iso_minish: dict, state: str, status: bool):
+        """Can validate a record against base schema."""
+        record = Record.loads(fx_record_config_iso_minish)
+        if not status:
+            record.identification.title = None
+        result = ToolValidate._validate_base(record)
+        assert isinstance(result, ValidationOutput)
+        assert result.status == status
+
+    @pytest.mark.parametrize(("state", "status"), [("error", False), ("valid", True), ("invalid", False)])
+    def test_validate_lantern(self, fx_record_config_cat_min: dict, state: str, status: bool):
+        """
+        Can validate a record against catalogue requirements.
+
+        Also handles where a record can't be loaded as a catalogue record due to properties which are optional in base
+        records but required in catalogue records being missing (e.g. file_identifier), triggering a load error.
+        """
+        record = Record.loads(fx_record_config_cat_min)
+        expected_error = "Record config does not meet [Lantern 🏮 Catalogue Requirements](https://github.com/antarctica/lantern/blob/main/docs/models.md#record-requirements) 😰"
+        if not status:
+            record.identification.identifiers = Identifiers([])
+        if state == "error":
+            record.file_identifier = None
+            expected_error = "Record cannot be parsed as a catalogue record and cannot be validated further 😵"
+
+        result = ToolValidate._validate_lantern(record)
+        assert isinstance(result, ValidationOutput)
+        assert result.status == status
+        if not status:
+            assert result.message == expected_error
+
+    @pytest.mark.parametrize(("state", "status"), [("valid", True), ("invalid", False)])
+    def test_validate_discovery(self, fx_record_config_magic: dict, state: str, status: bool):
+        """Can validate a record against MAGIC discovery profile."""
+        record = Record.loads(fx_record_config_magic)
+        if not status:
+            record.identification.edition = None
+        result = ToolValidate._validate_discovery(record)
+        assert isinstance(result, ValidationOutput)
+        assert result.status == status
+
+    @pytest.mark.parametrize(("state", "status"), [("valid", True), ("invalid", False)])
+    def test_validate_admin(self, fx_record_config_magic: dict, state: str, status: bool):
+        """Can validate a record against MAGIC administration profile."""
+        record = Record.loads(fx_record_config_magic)
+        if not status:
+            record.identification.supplemental_information = None
+        result = ToolValidate._validate_admin(record)
+        assert isinstance(result, ValidationOutput)
+        assert result.status == status
+
+
+class TestValidateSectionUI:
+    """Test validation tool UI."""
 
     @staticmethod
     def _app_script() -> None:
@@ -34,68 +132,50 @@ class TestValidateSection:
         at.run()
 
         assert not at.exception
-        assert at.info[0].value == "Set record config to enable validation."
+        assert at.info[0].value == "Set record to enable validation."
         # should have no other output
         assert len(at.success) == 0
         assert len(at.error) == 0
         assert len(at.exception) == 0
 
-    @pytest.mark.parametrize(
-        "state", ["invalid-iso", "valid-iso", "invalid-cat", "valid-cat", "invalid-magic", "valid-magic"]
-    )
-    def test_validation(
-        self, fx_record_config_iso_min: dict, fx_record_config_cat_min: dict, fx_record_config_magic: dict, state: str
-    ):
-        """Can show correct state depending on current record."""
-        config = fx_record_config_iso_min if "iso" in state else fx_record_config_cat_min
-        config = fx_record_config_magic if "magic" in state else config
+    @pytest.mark.parametrize("state", ["all-invalid", "all-valid"])
+    def test_validation(self, fx_record_config_iso_minish: dict, fx_record_config_magic: dict, state: str):
+        """
+        Can show correct state depending on current record.
+
+        Acts as an integration test for all checks. An invalid ISO record should stop further validation.
+        """
+        config = fx_record_config_iso_minish if "iso" in state else fx_record_config_magic
         record = Record.loads(config)
-        # remove required properties if needed
-        if state == "invalid-iso":
+        if state == "all-invalid":
             record.identification.title = None
-        if state == "invalid-cat":
-            record.identification.identifiers = Identifiers([])
-        if state == "invalid-magic":
-            record.identification.supplemental_information = None
 
         at = AppTest.from_function(self._app_script)
         at.session_state.record = record
         at.run()
 
         assert len(at.info) == 0
-        if state == "invalid-iso":
-            assert at.error[0].value == "Record config is invalid 😞"
+        if state == "all-invalid":
+            assert at.error[0].value == "Record config is invalid 😭"
             # should have no other output
             assert len(at.error) == 1
             assert len(at.exception) == 1
             assert len(at.success) == 0
-        elif state == "valid-iso":
+        elif state == "all-valid":
             assert at.success[0].value == "Record config is valid 🙂"
-            # should still fail catalogue and profile validation
-            assert len(at.error) > 0
-            assert len(at.exception) > 0
-            assert len(at.success) == 1
-        elif state == "invalid-cat":
-            assert at.error[0].value == "Record config does not meet catalogue requirements 😢"
-            assert len(at.exception) > 0
-            # should still pass ISO validation
-            assert len(at.success) == 1
-        elif state == "valid-cat":
-            assert at.success[0].value == "Record config is valid 🙂"
-            assert at.success[1].value == "Record config meets catalogue requirements 😀"
-            # should still fail profile validation
-            assert len(at.error) > 0
-            assert len(at.exception) > 0
-            assert len(at.success) == 2  # noqa: PLR2004
-        elif state == "invalid-magic":
-            assert at.error[0].value == "Record config does not meet MAGIC profile requirements 😭"
-            assert len(at.exception) > 0
-            # should still pass ISO and catalogue validation
-            assert len(at.success) == 2  # noqa: PLR2004
-        elif state == "valid-cat":
-            assert at.success[0].value == "Record config is valid 🙂"
-            assert at.success[1].value == "Record config meets catalogue requirements 😀"
-            assert at.success[2].value == "Record config meets MAGIC profile requirements 🥳"
+            assert (
+                at.success[1].value
+                == "Record config meets [Lantern 🏮 Catalogue Requirements](https://github.com/antarctica/lantern/blob/main/docs/models.md#record-requirements) 😀"
+            )
+            assert (
+                at.success[2].value
+                == "Record config meets the [MAGIC Discovery Profile](https://metadata-standards.data.bas.ac.uk/profiles/magic-discovery/v2) 🤩"
+            )
+            assert (
+                at.success[3].value
+                == "Record config meets the [MAGIC Administration Profile](https://metadata-standards.data.bas.ac.uk/profiles/magic-administration/v1/) 🥳"
+            )
+
             assert len(at.error) == 0
             assert len(at.exception) == 0
-            assert len(at.success) == 3  # noqa: PLR2004
+            assert len(at.success) == 4  # noqa: PLR2004
